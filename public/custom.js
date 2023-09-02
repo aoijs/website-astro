0f0e3b1accbb8d6aea3662ba2f097f9a6440883c
Fabrice Fesser <87046111+Faf4a@users.noreply.github.com>
2023-09-02 02:49:03 +0200

    feat: copy button for codeblocks

--- public/custom.js
+++ public/custom.js
@@ -1,38 +1,69 @@
-const key = 'toc-position';
+//handle toc update / keep focus (ignore)
+const key = "toc-position";
 let position = { scrollTop: 0 };
-const sidebar = document.querySelector('#starlight__sidebar > .sidebar-content');
+const sidebar = document.querySelector(
+  "#starlight__sidebar > .sidebar-content"
+);
 
 if (sidebar) {
-    const data = sessionStorage.getItem(key);
-    if (data) {
-        try {
-            const obj = JSON.parse(data);
-            position = {
-                ...position,
-                ...obj,
-            };
-            sidebar.scrollTop = position.scrollTop || 0;
-        } catch (err) {
-            console.error("[ERR] ", err);
-        }
+  const data = sessionStorage.getItem(key);
+  if (data) {
+    try {
+      const obj = JSON.parse(data);
+      position = {
+        ...position,
+        ...obj,
+      };
+      sidebar.scrollTop = position.scrollTop || 0;
+    } catch (err) {
+      console.error("[ERR] ", err);
     }
+  }
 
-    sidebar.addEventListener('scroll', () => {
-        position.scrollTop = sidebar.scrollTop;
-        sessionStorage.setItem(key, JSON.stringify(position));
-    });
+  sidebar.addEventListener("scroll", () => {
+    position.scrollTop = sidebar.scrollTop;
+    sessionStorage.setItem(key, JSON.stringify(position));
+  });
 }
 
 new MutationObserver((mutations) => {
-    for (const mutation of mutations) {
-        if (mutation.type === "attributes") {
-            const current = mutation.target.getAttribute("aria-current");
-            if (current) {
-                mutation.target.scrollIntoView({ block: "center" });
-            }
-        }
+  for (const mutation of mutations) {
+    if (mutation.type === "attributes") {
+      const current = mutation.target.getAttribute("aria-current");
+      if (current) {
+        mutation.target.scrollIntoView({ block: "center" });
+      }
     }
+  }
 }).observe(document.querySelector("starlight-toc > nav > ul"), {
-    attributeFilter: ["aria-current"],
-    subtree: true,
+  attributeFilter: ["aria-current"],
+  subtree: true,
 });
+
+// codeblock copy button (ignore)
+let codeBlocks = Array.from(document.querySelectorAll("pre"));
+
+for (let codeBlock of codeBlocks) {
+  let wrapper = document.createElement("div");
+  wrapper.style.position = "relative";
+  let copyButton = document.createElement("div");
+  copyButton.className = "copy-code";
+  copyButton.innerHTML = `<img src="/website-astro/icons/copy.png">`;
+
+  copyButton.addEventListener("click", async () => {
+    await copyCode(codeBlock, copyButton);
+  });
+
+  wrapper.appendChild(copyButton);
+  codeBlock.parentNode.insertBefore(wrapper, codeBlock);
+}
+
+async function copyCode(block, button) {
+  let code = block.querySelector("code");
+  let text = code.innerText;
+  await navigator.clipboard.writeText(text);
+  button.innerHTML = `<img src="/website-astro/icons/check.png">`;
+  setTimeout(() => {
+    button.innerHTML = `<img src="/website-astro/icons/copy.png">`;
+  }, 700);
+}
