7fb0d9f41c8927553a1aa1f4af0968c3afb574ab
Fabrice Fesser <87046111+Faf4a@users.noreply.github.com>
2023-09-09 13:09:07 +0200

    fix: sidebar collapse

--- public/custom.js
+++ public/custom.js
@@ -1,51 +1,84 @@
-// handle toc update / keep focus (ignore)
-const key = "toc-position";
-let position = { scrollTop: 0 };
-const sidebar = document.querySelector(
-  "#starlight__sidebar > .sidebar-content",
-);
+(() => {
 
-if (sidebar) {
-  const data = sessionStorage.getItem(key);
-  if (data) {
-    try {
-      const obj = JSON.parse(data);
-      position = {
-        ...position,
-        ...obj,
-      };
-      sidebar.scrollTop = position.scrollTop || 0;
-    } catch (err) {
-      console.error("[ERR] ", err);
-    }
-  }
+  const key = 'toc-position';
+  
+  let timeout = 0;
 
-  sidebar.addEventListener("scroll", () => {
-    position.scrollTop = sidebar.scrollTop;
-    sessionStorage.setItem(key, JSON.stringify(position));
-  });
-}
+  /** @type {{ scrollTop: number, details: boolean[] }} */
+  let position = { scrollTop: 0, details: [] };
 
-// Create a new MutationObserver
-const observer = new MutationObserver((mutations) => {
-  for (const mutation of mutations) {
-    if (mutation.type === "attributes") {
-      const current = mutation.target.getAttribute("aria-current");
-      if (current) {
-        mutation.target.scrollIntoView({ block: "center" });
+  const sidebar = document.querySelector('#starlight__sidebar > .sidebar-content');
+
+  if (sidebar) {
+    const details = Array.from(sidebar.querySelectorAll('details'));
+    position.details = details.map(detail => detail.hasAttribute('open'));
+
+    const data = sessionStorage.getItem(key);
+    if (data) {
+      try {
+        const obj = JSON.parse(data);
+        position = {
+          ...position, ...obj,
+        };
+
+        const pathname = document.location.pathname;
+        const query = `[href="${pathname}/"]`;
+
+        let target = undefined;
+
+        for (const [index, entry] of obj.details.entries() || []) {
+
+          const q = details[index].querySelector(query);
+          if (q) {
+            target = q;
+          }
+
+          if (entry || !!q) {
+            details[index].setAttribute('open', 'true');
+            if (q) {
+              q.setAttribute('aria-current', 'page');
+              q.scrollIntoView();
+            }
+          }
+          else {
+            details[index].removeAttribute('open');
+          }
+        }
+
+        if (target) {
+          target.scrollIntoView({behavior: 'smooth'});
+        }
+        else {
+          sidebar.scrollTop = position.scrollTop || 0;
+        }
+
+      }
+      catch (err) {
+        console.error(err);
       }
     }
-  }
-});
 
-// Ensure the element exists before attempting to observe it
-const targetElement = document.querySelector("starlight-toc > nav > ul");
-if (targetElement) {
-  observer.observe(targetElement, {
-    attributeFilter: ["aria-current"],
-    subtree: true,
-  });
-}
+    const Store = () => {
+      if (timeout) {
+        window.clearTimeout(timeout);
+      }
+      timeout = window.setTimeout(() => {
+        position.scrollTop = sidebar.scrollTop;
+        position.details = details.map(detail => detail.hasAttribute('open'));
+        sessionStorage.setItem(key, JSON.stringify(position));
+        timeout = 0;
+      }, 1000);
+    };
+
+    sidebar.addEventListener('scroll', Store);
+
+    for (const detail of details) {
+      detail.addEventListener('toggle', Store);
+    }
+
+  }
+
+})();
 
 // codeblock copy button (ignore)
 let codeBlocks = Array.from(document.querySelectorAll("pre"));
